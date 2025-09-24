cf06bb33331f0398806bf13f4a017b6d4ab24965
Miltonnare <miltonnareblessed@gmail.com>
2025-09-24 21:49:23 +0300

    Assign Updates

---
+++ nmm.js
@@ -0,0 +1,20 @@
+for (let i=0;i<3;i++){
+console.log("Python vs Js")
+
+};
+
+// Python Script to Input two numbers and Sum them up 
+ num1=int(input("Enter the First Number: "));
+ num2=int(input("Enter the Second Number: "));
+
+ result=num1*num2;
+ print(result);
+
+
+//  Javascript Script to input two numbers and Sum them  up
+
+const num1 =parseInt(prompt("Enter the First Number: "));
+const num2=parseInt(prompt("Enter the Second Number: "));
+
+const result=num1*num2;
+console.log(result);
