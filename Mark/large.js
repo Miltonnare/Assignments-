765c6913dc6bbca558aea8bfaae50ffe561ae89e
Miltonnare <miltonnareblessed@gmail.com>
2025-09-27 22:29:03 +0300

    Updated for js

---
+++ Mark/large.js
@@ -0,0 +1,23 @@
+
+const array=[5,4,6,7,10];
+
+function findLargest(){
+    let largerst=array[0];
+
+    for (let i=1;i<array.length;i++){
+
+        if(array[i]>largerst){
+             largerst=array[i];
+
+             
+        }
+       
+
+        
+
+    }
+    return largerst;
+}
+
+console.log("The Result: ",findLargest());
+
