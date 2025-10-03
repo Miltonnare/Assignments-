1b78a74db8d0749c91474b495a4e9771bea6435a
Miltonnare <miltonnareblessed@gmail.com>
2025-10-03 15:55:31 +0300

    php Basics

---
+++ Rennaisence/js.php
@@ -0,0 +1,28 @@
+
+<?php
+    $hajime="Iam the most outstanding Guy";
+    echo "Trying is learning and not failing";
+    echo $sojura="Iam the lover of the adaptable mankind";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>
+    <?php
+     echo $hajime;
+     ?>
+     </h1> 
+
+<script type="text/javascript">
+    let name="<?php echo $hajime;?>";
+    alert(name);
+    
+</script>
+</body>
+</html>
