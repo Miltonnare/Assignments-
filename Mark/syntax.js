1d2541dddc4c74446e8d7e3d95a4e1fee9824e02
Miltonnare <miltonnareblessed@gmail.com>
2025-10-09 21:15:09 +0300

    Don't mind

---
+++ Mark/syntax.js
@@ -0,0 +1,34 @@
+
+console.log("Hello World")
+print("Hello World")
+
+let x=5;
+const y=6;
+
+x=5
+y=6
+
+if(hungry){
+    console.log("Iam Hungry");
+}
+// Learning Syntaxes in js and Python
+if hungry:
+    print("Iam Hungry")
+
+if x>10:
+    print("Gr")
+
+
+for i in Range(5):
+    print(i)
+
+
+function Multp(){
+
+
+
+}
+
+def Multp(a,b):
+    return a*b
+    
