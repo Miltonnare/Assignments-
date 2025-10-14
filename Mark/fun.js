84f8d083d05840ffb1dd638e6ed3303a54389f49
Miltonnare <miltonnareblessed@gmail.com>
2025-10-14 22:49:51 +0300

    Don't mind

---
+++ Mark/fun.js
@@ -0,0 +1,49 @@
+num3=56
+
+function Add(){
+
+    let a=4;
+    let b=20
+    result=a+b;
+
+    console.log(result);
+
+    // code executed
+}
+
+
+Add();
+
+function Add(a,b){
+    result=a+b;
+    console.log(result);
+}
+
+Add(20,9);
+
+Add(89,10);
+
+Add(8,20);
+Add(50,7);
+
+
+// function Add(a,b){
+    
+//     return a+b;
+
+// }
+
+//  console.log("The result is",Add(5,6));
+
+
+//  let a=10;
+//  let b=20;
+//  let result=a+b;
+
+//  console.log(result)
+
+
+//  a=50;
+//  b=7;
+
+//  console.log(result);
---
+++ Mark/fun.py
@@ -0,0 +1,24 @@
+
+def Multiply(num1,num2):
+
+    return num1*num2
+
+
+
+result=Multiply(7,10)
+
+
+print(f"The Result is: {result}")
+
+
+def Multiply():
+    num1=4
+    num2=5
+
+    result=num1*num2
+    print(result)
+
+
+
+Multiply()
+
