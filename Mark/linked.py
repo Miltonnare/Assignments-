e348193da0cd31d5d3662922870877dc7b1ed24e
Miltonnare <miltonnareblessed@gmail.com>
2025-11-04 21:07:26 +0300

    Added

---
+++ Mark/linked.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.display()
+
+
+#[1,2,3,4,5]
+
+# array[3]
+#array[178]
+#array[2]
+
