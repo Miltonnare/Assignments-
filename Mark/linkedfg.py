f81ae6f227141b684d23d6507dd2945ae0a944a9
Miltonnare <miltonnareblessed@gmail.com>
2025-11-05 22:16:41 +0300

    Update

---
+++ Mark/linkedfg.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Linkedlist:
+
+    def __init__(self):
+        self.head = None
+
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_list(self):
+        current = self.head
+
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+
+Rachel = Linkedlist()
+Rachel.append("Apple")
+Rachel.append("Banana")
+Rachel.append("Cherry")
+Rachel.print_list()
