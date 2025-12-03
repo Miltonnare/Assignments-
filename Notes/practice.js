de1ae60a7fc1c8d87c98beb6d06a703962d52bbc
Miltonnare <miltonnareblessed@gmail.com>
2025-12-03 19:37:01 +0300

    exmas

--- Notes/practice.js
+++ Notes/practice.js
@@ -11,3 +11,45 @@ function Sub(a,b){
 
 Add(5,6);
 Sub(7,8);
+
+
+for (let i=0;i<=5;i++){
+    if(i===3){
+        continue;
+    }
+    // console.log(i)
+
+    if (i===4){
+        break;
+    }
+     console.log(i)
+
+}
+
+
+try {
+    for (let i=0;i<=5;i++){
+    if(i===3){
+        continue;
+    }
+
+    if(i===2){
+        throw new error ("Expected errros");
+    }
+    
+
+    if (i===4){
+        break;
+    }
+     console.log(i)
+
+}
+} catch (error) {
+    console.error("Caught an Error", error.message)
+    
+}
+
+let i=5;
+while(i<4){
+    console.log("I love You!");
+}
