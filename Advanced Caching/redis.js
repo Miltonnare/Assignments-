e71f6d499b79e24324b87247fb2494dba4423f8a
Miltonnare <miltonnareblessed@gmail.com>
2025-12-15 11:01:25 +0300

    redis

--- Advanced Caching/redis.js
+++ Advanced Caching/redis.js
@@ -9,6 +9,21 @@ const REDIS_PORT=process.env.PORT||6379;
 const client=redis.createClient(REDIS_PORT);
 
 const app=express();
+
+async function getRepos(req,res,next){
+    try {
+        console.log('Fetching Data...')
+        const {username}=req.params;
+
+        const reponse=await fetch(`https://api.github.com/users/${username}`);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        
+    }
+
+}
 app.get('/repos/:username', getRepos)
 
 app.listen(PORT,()=>{
