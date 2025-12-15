a5fcee6888744f6995b28d5891986f577e0c3db2
Miltonnare <miltonnareblessed@gmail.com>
2025-12-15 11:13:06 +0300

    Redis in nodejs

--- Advanced Caching/redis.js
+++ Advanced Caching/redis.js
@@ -10,12 +10,22 @@ const client=redis.createClient(REDIS_PORT);
 
 const app=express();
 
+function setResponse(username,repos){
+    return`<h2>${username} has ${repos} Github Repos</h2>`
+}
+
 async function getRepos(req,res,next){
     try {
         console.log('Fetching Data...')
         const {username}=req.params;
 
         const reponse=await fetch(`https://api.github.com/users/${username}`);
+        const data=await response.json();
+        const repos=data.public_repos;
+        //Give the data to redis
+
+        client.setex(username,3600,repos);
+        res.send(setResponse(username,repos))
         
     } catch (error) {
         console.error(error);
@@ -24,7 +34,20 @@ async function getRepos(req,res,next){
     }
 
 }
-app.get('/repos/:username', getRepos)
+//Cache Middleware
+function cache(req,res,next){
+    const {username}=req.params;
+    client.get(username,(err,data)=>{
+        if(err) throw err;
+
+        if(data!==null){
+            res.send(setResponse(username,data));
+        }else{
+            next();
+        }
+    })
+}
+app.get('/repos/:username',cache, getRepos)
 
 app.listen(PORT,()=>{
 console.log(`App listening on Port ${PORT}`);
