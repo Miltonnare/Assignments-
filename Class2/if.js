e9f25a628e0e3d2ef63c2f1488d4f9252e5cd6a4
Miltonnare <miltonnareblessed@gmail.com>
2025-12-17 21:52:31 +0300

    mmm

---
+++ Class2/if.js
@@ -0,0 +1,32 @@
+
+let names="John";
+
+if(names=="Joseph"){
+    console.log("The Best");
+}
+
+else{
+    console.log("The name is not correct");
+}
+
+let age=20;
+
+if(age<18){
+    console.log("You are UnderAge");
+}
+else{
+    console.log("You are over 18");
+}
+
+
+let weather = "snowy";
+
+if (weather === "rainy") {
+    console.log("Take an umbrella");
+} else if (weather === "sunny") {
+    console.log("Wear sunglasses");
+} else if (weather === "snowy") {
+    console.log("Wear a coat");
+} else {
+    console.log("Stay inside");
+}
---
+++ Class2/if.py
@@ -0,0 +1,18 @@
+
+age=16
+
+if age<18:
+    print("You are UnderAge")
+
+
+
+weather = "snowy"
+
+if weather == "rainy":
+    print("Take an umbrella")
+elif weather == "sunny":
+    print("Wear sunglasses")
+elif weather == "snowy":
+    print("Wear a coat")
+else:
+    print("Stay inside")
