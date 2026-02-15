5bb0c57e9f3521a8eb0cb7081fc46bb5fec14a3b
Miltonnare <miltonnareblessed@gmail.com>
2026-02-15 13:54:45 +0300

    routes

--- LearningApp/routes/web.php
+++ LearningApp/routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('app');
 })->name('home');
 
+Route::get('/job', function () {
+    return view('app');
+})->name('job.index');
 
-Route::get("/job",[JobController::class,'index']
-)->name('job.index');
+Route::get('/login', fn () => view('app'))->name('login');
+Route::get('/register', fn () => view('app'))->name('register');
