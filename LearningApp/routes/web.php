fe5bad1063a578a5e7adcf041a7713acc718b672
Miltonnare <miltonnareblessed@gmail.com>
2026-02-15 13:40:35 +0300

    web

--- LearningApp/routes/web.php
+++ LearningApp/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 })->name('home');
+
+
+Route::get("/job",[JobController::class,'index']
+)->name('job.index');
