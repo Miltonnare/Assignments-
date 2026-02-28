e99de4bd8c3e8a7255dc3012d35b586f39eb995f
Miltonnare <miltonnareblessed@gmail.com>
2026-02-28 21:17:25 +0300

    Blade View

--- LearningApp/app/Models/User.php
+++ LearningApp/app/Models/User.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-        use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     public const ROLE_CLIENT = 'client';
     public const ROLE_FREELANCER = 'freelancer';
